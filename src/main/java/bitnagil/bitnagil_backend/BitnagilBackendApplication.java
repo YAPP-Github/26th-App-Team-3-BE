6a0648e463abeb74f5e6bfe8f90ea22447e374ef
정유석 <elwlahstmxjf@naver.com>
2025-06-02 00:28:14 +0900

    intial commit

---
+++ src/main/java/bitnagil/bitnagil_backend/BitnagilBackendApplication.java
@@ -0,0 +1,13 @@
+package bitnagil.bitnagil_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitnagilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitnagilBackendApplication.class, args);
+	}
+
+}
