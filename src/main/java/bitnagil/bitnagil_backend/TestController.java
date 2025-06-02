7353c1a293e12936efcf84d862f5b570b35ccaf0
정유석 <elwlahstmxjf@naver.com>
2025-06-02 21:11:22 +0900

    feat: change endpoint for health check

--- src/main/java/bitnagil/bitnagil_backend/TestController.java
+++ src/main/java/bitnagil/bitnagil_backend/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/health/check")
+    @GetMapping("/health-check")
     public String newURI() {
         return "Github Actions로 배포 성공";
     }
