84401c16dbfcd89cea5c974ea825142e6e4ef92d
thisishwan2 <feel000617@gmail.com>
2025-06-06 02:36:22 +0900

    chore: test cicd with submodule

--- src/main/java/bitnagil/bitnagil_backend/HealthCheckController.java
+++ src/main/java/bitnagil/bitnagil_backend/HealthCheckController.java
@@ -1,5 +1,6 @@
 package bitnagil.bitnagil_backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
+    @Value("${spring.port}")
+    private String port;
+
+    @Value("${spring.config.active.on-profile}")
+    private String activeProfile;
+
+
     @GetMapping("/health-check")
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("OK-과연...!");
+        return ResponseEntity.ok("pot: " + port + ", active profile: " + activeProfile);
     }
 }
