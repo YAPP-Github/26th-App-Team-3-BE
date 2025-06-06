ad38005414a77d42632461390a30e4c73653f20e
thisishwan2 <feel000617@gmail.com>
2025-06-06 14:09:08 +0900

    chore: local 환경과 ci 환경에서 모두 yml을 찾도록 수정

--- src/main/java/bitnagil/bitnagil_backend/HealthCheckController.java
+++ src/main/java/bitnagil/bitnagil_backend/HealthCheckController.java
@@ -1,6 +1,7 @@
 package bitnagil.bitnagil_backend;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,15 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @Value("${spring.port}")
+    @Value("${server.port:unknown}")
     private String port;
 
-    @Value("${spring.config.active.on-profile}")
-    private String activeProfile;
+    private final Environment environment;
 
+    public HealthCheckController(Environment environment) {
+        this.environment = environment;
+    }
 
     @GetMapping("/health-check")
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("pot: " + port + ", active profile: " + activeProfile);
+        String activeProfile = String.join(", ", environment.getActiveProfiles());
+        return ResponseEntity.ok("port: " + port + ", active profile: " + activeProfile);
     }
 }
