4338bd9d7d62041d098b6e2212ec5fe4025454a3
정유석 <elwlahstmxjf@naver.com>
2025-06-09 22:50:05 +0900

    feat: add security configuration

---
+++ src/main/java/bitnagil/bitnagil_backend/jwt/service/UserAuthentication.java
@@ -0,0 +1,13 @@
+package bitnagil.bitnagil_backend.jwt.service;
+
+import java.util.Collection;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+public class UserAuthentication extends UsernamePasswordAuthenticationToken {
+
+    public UserAuthentication(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(principal, credentials, authorities);
+    }
+}
